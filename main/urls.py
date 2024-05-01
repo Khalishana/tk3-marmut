6c09ec7791eb8407034116d22e5763332cce1a06
khalishana <khalisha.hana10@gmail.com>
2024-05-01 23:12:17 +0700

    feat: add authentication and landing page

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from main.views import show_main
+from authentication.views import show_landing
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', show_main, name='show_main'),
+    path('main/', show_main, name='show_main'),
+    # path('', show_landing, name='show_landing'),
+    #path('', include('authentication.urls'))
 ]

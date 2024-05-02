84ce6431b36008827acd4d76900a56c34d3fa352
khalishana <khalisha.hana10@gmail.com>
2024-05-02 19:42:41 +0700

    style: add white background

--- marmut/settings.py
+++ marmut/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'authentication',
+    'downloaded_songs',
+    'kelola_playlist',
+    'play_song',
+    'play_user_playlist',
 ]
 
 MIDDLEWARE = [

a01f5a07d9a7de5aa17c6b97646d379d612b70ba
khalishana <khalisha.hana10@gmail.com>
2024-05-02 14:20:35 +0700

    feat: add downloaded songs and search bar

--- marmut/urls.py
+++ marmut/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authentication.urls')),
+    path('downloaded/', include('downloaded_songs.urls')),
+    path('search/', include('downloaded_songs.urls'))
 ]

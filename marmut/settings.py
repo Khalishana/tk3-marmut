22289acec38e128277f3183b075c19f5cb4a1962
khalishana <khalisha.hana10@gmail.com>
2024-05-15 10:56:42 +0700

    connect to supabase

--- marmut/settings.py
+++ marmut/settings.py
@@ -86,10 +86,21 @@ WSGI_APPLICATION = 'marmut.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
+        'NAME': 'postgres',
+        'USER': 'postgres.vjxypfaouaiqkavqanuu',
+        'PORT': '5432',
+        'PASSWORD': 'marmutkelompok9'
     }
 }
 

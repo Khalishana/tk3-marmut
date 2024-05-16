82482b44dc09fbb555c298bb071c58501ece024f
khalishana <khalisha.hana10@gmail.com>
2024-05-16 12:58:13 +0700

    feat: connect langganan to database

--- langganan/urls.py
+++ langganan/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from langganan.views import show_langganan, show_pembayaran, show_riwayat
+from langganan.views import show_langganan, show_pembayaran, show_riwayat, show_transactions, show_paket_langganan
 
 app_name = 'langganan'
 
 urlpatterns = [
-    path('', show_langganan, name='show_langganan'),
+    #path('', show_langganan, name='show_langganan'),
+    path('', show_paket_langganan, name='show_paket_langganan'),
     path('pembayaran/', show_pembayaran, name='show_pembayaran'),
-    path('riwayat/', show_riwayat, name='show_riwayat'),
+    #path('riwayat/', show_riwayat, name='show_riwayat'),
+    path('riwayat/', show_transactions, name='show_transactions'),
 ]
--- langganan/views.py
+++ langganan/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db import connection
 
 # Create your views here.
 def show_langganan(request):
@@ -21,3 +22,33 @@ def show_riwayat(request):
         'class': 'Basdat A'
     }
     return render(request, "riwayat.html", context)
+
+def show_transactions(request):
+    with connection.cursor() as cursor:
+        cursor.execute("SET search_path TO marmut;")
+        cursor.execute("""
+            SELECT jenis_paket, email, timestamp_dimulai, timestamp_berakhir, metode_bayar, nominal 
+            FROM transaction
+        """)
+        transactions = cursor.fetchall()
+
+    print(transactions)
+
+    context = {
+        'transactions': transactions
+    }
+    return render(request, 'riwayat.html', context)
+
+def show_paket_langganan(request):
+    with connection.cursor() as cursor:
+        cursor.execute("SET search_path TO marmut;")
+        cursor.execute("""
+            SELECT jenis, harga
+            FROM paket
+        """)
+        pakets = cursor.fetchall()
+
+    context = {
+        'pakets': pakets
+    }
+    return render(request, 'langganan.html', context)

3439bef6ac0346301b2a12a10a058059419a5195
Fadrian Yhoga Pratama <fadrian.yhoga@ui.ac.id>
2024-05-19 00:49:05 +0700

    fix: add csrf trusted origins

--- marmut/settings.py
+++ marmut/settings.py
@@ -149,3 +149,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://web-production-8be4.up.railway.app/']

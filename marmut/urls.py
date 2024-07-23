5134b3296ce3f1eb9904c887ae99a4a1a7c39c8c
khalishana <khalisha.hana10@gmail.com>
2024-07-23 12:31:40 +0700

    style: add images

--- marmut/urls.py
+++ marmut/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -35,3 +37,5 @@ urlpatterns = [
     path('melihat_chart/', include('melihat_chart.urls')),
     path('play_podcast/', include('play_podcast.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
